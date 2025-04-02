87699e62a710144aba1d33bb020aa252e19e4a26
DSA <dsa@mtb111.fr>
2025-04-02 14:59:15 +0200

    Spinner en + pour les URL de connexion

---
+++ app/src/main/java/com/example/appli20240829/DonneesPartagees.java
@@ -0,0 +1,15 @@
+package com.example.appli20240829;
+
+public class DonneesPartagees {
+    private static String URLConnexion;
+
+    public void DonneesPartagees() {
+
+    }
+    public static void setURLConnexion(String URL) {
+        URLConnexion = URL;
+    }
+    public static String getURLConnexion() {
+        return URLConnexion;
+    }
+}
--- app/src/main/java/com/example/appli20240829/LoginActivity.java
+++ app/src/main/java/com/example/appli20240829/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.appli20240829;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,16 +16,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+//SPINNER URLs-D
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter ;
+import android.widget.Toast;
+//SPINNER URLs-F
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private EditText usernameField, passwordField;
     private Button loginButton;
+    String[] listeURLs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //SPINNER URLs-D
+        listeURLs = getResources().getStringArray(R.array.listeURLs);
+        Spinner spinnerURLs=findViewById(R.id.spinnerURLs);
+        spinnerURLs.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence>adapterListeURLs=ArrayAdapter.createFromResource(this, R.array.listeURLs, android.R.layout.simple_spinner_item);
+        adapterListeURLs.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerURLs.setAdapter(adapterListeURLs);
+        //SPINNER URLs-F
 
         usernameField = findViewById(R.id.username);
         passwordField = findViewById(R.id.password);
@@ -46,7 +63,7 @@ public class LoginActivity extends AppCompatActivity {
         new Thread(() -> {
             try {
                 // URL mise à jour avec le bon chemin
-                String apiUrl = "http://10.0.2.2:8080/toad/customer/getByEmail?email=" + email;
+                String apiUrl = DonneesPartagees.getURLConnexion() + "/toad/customer/getByEmail?email=" + email;
                 URL url = new URL(apiUrl);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
@@ -85,4 +102,20 @@ public class LoginActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    // jsp il sert a quoi ce code mais ça marche donc tranquille
+    //SPINNER URLs-D
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // Make toast of the name of the course which is selected in the spinner
+        //Toast.makeText(getApplicationContext(), listeURLs[position], Toast.LENGTH_SHORT).show();
+        DonneesPartagees.setURLConnexion(listeURLs[position]);
+        Toast.makeText(getApplicationContext(), DonneesPartagees.getURLConnexion(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // No action needed when no selection is made
+    }
+    //SPINNER URLs-F
 }
